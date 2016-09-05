838b62a085d72aaf22ad4f5cdb8574cf51bf3470
JulioBarahona <julio.fbm96@gmail.com>
2016-09-04 19:49:23 -0600

    its dione mr frodo
    GUI finilized, now TO MORDOR!

--- ImplementationType.java
+++ ImplementationType.java
@@ -44,6 +44,7 @@ public class ImplementationType extends javax.swing.JFrame {
         developerNameInputtf = new javax.swing.JTextField();
         showDevsbtn = new javax.swing.JToggleButton();
         developerlbl = new javax.swing.JLabel();
+        testlbl = new javax.swing.JLabel();
         finishInputbtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -108,6 +109,14 @@ public class ImplementationType extends javax.swing.JFrame {
 
         developerlbl.setText("Developer Name");
 
+        finishInputbtn.setText("End Registration");
+        finishInputbtn.setEnabled(false);
+        finishInputbtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                finishInputbtnActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -132,10 +141,21 @@ public class ImplementationType extends javax.swing.JFrame {
                                 .addComponent(mobileDevTxtBx)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(javaDevTxtBx)))
-                        .addGap(82, 82, 82)
-                        .addComponent(registerbtn)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 72, Short.MAX_VALUE)
-                        .addComponent(showDevsbtn)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(finishInputbtn)
+                                .addGap(133, 133, 133))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(82, 82, 82)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(testlbl)
+                                        .addGap(0, 0, Short.MAX_VALUE))
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(registerbtn)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 72, Short.MAX_VALUE)
+                                        .addComponent(showDevsbtn)))))
                         .addGap(45, 45, 45))))
         );
         jPanel1Layout.setVerticalGroup(
@@ -143,93 +163,69 @@ public class ImplementationType extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(implementationTypelbl))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(implementationTypelbl))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(16, 16, 16)
+                                .addComponent(developerlbl)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(implementationCmb, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(16, 16, 16)
-                        .addComponent(developerlbl)))
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(17, 17, 17)
+                        .addGap(27, 27, 27)
+                        .addComponent(testlbl)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(developerNameInputtf, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(registerbtn)
-                            .addComponent(showDevsbtn)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(implementationCmb, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(showDevsbtn))))
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(53, 53, 53)
                         .addComponent(implementationbtn))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(35, 35, 35)
+                        .addGap(31, 31, 31)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(webDevTxtBx)
                             .addComponent(mobileDevTxtBx)
-                            .addComponent(javaDevTxtBx))))
+                            .addComponent(javaDevTxtBx)
+                            .addComponent(finishInputbtn))))
                 .addContainerGap(34, Short.MAX_VALUE))
         );
-
-        finishInputbtn.setText("End Registration");
-        finishInputbtn.setEnabled(false);
-        finishInputbtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                finishInputbtnActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(475, Short.MAX_VALUE)
-                .addComponent(finishInputbtn)
-                .addGap(198, 198, 198))
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addGap(0, 0, Short.MAX_VALUE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(0, 0, Short.MAX_VALUE)))
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(113, Short.MAX_VALUE)
-                .addComponent(finishInputbtn)
-                .addGap(59, 59, 59))
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addGap(0, 0, Short.MAX_VALUE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(0, 0, Short.MAX_VALUE)))
+                .addGap(28, 28, 28)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void finishInputbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_finishInputbtnActionPerformed
-        // TODO add your handling code here:
-        implementationbtn.      setEnabled(false);             
-        showDevsbtn.            setEnabled(true);
-        javaDevTxtBx.           setEnabled(false);
-        webDevTxtBx.            setEnabled(false);
-        mobileDevTxtBx.         setEnabled(false);
-        developerNameInputtf.   setEnabled(false);
-        registerbtn.            setEnabled(false);        
-        finishInputbtn.         setEnabled(false);
-    }//GEN-LAST:event_finishInputbtnActionPerformed
-
     private void developerNameInputtfActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_developerNameInputtfActionPerformed
         // TODO add your handling code here:
+        while (developerNameInputtf.getText().equals("")) {
+            registerbtn.        setEnabled(false);
+            
+        }
     }//GEN-LAST:event_developerNameInputtfActionPerformed
 
     private void javaDevTxtBxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_javaDevTxtBxActionPerformed
         // TODO add your handling code here:
-        if (   webDevTxtBx.     isSelected() == false && 
-               javaDevTxtBx.    isSelected()== false &&
-               mobileDevTxtBx.  isSelected() == false
+        if (    webDevTxtBx.     isSelected() == false   && 
+                javaDevTxtBx.    isSelected()== false    &&
+                mobileDevTxtBx.  isSelected() == false
                 ) {
             registerbtn.        setEnabled(false);
         }
@@ -246,7 +242,6 @@ public class ImplementationType extends javax.swing.JFrame {
         mobileDevTxtBx.         setEnabled(true);
         developerNameInputtf.   setEnabled(true);
         implementationbtn.      setEnabled(false);
-        finishInputbtn.         setEnabled(true);
         
     }//GEN-LAST:event_implementationbtnActionPerformed
 
@@ -256,14 +251,31 @@ public class ImplementationType extends javax.swing.JFrame {
 
     private void registerbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_registerbtnActionPerformed
         // TODO add your handling code here:
-        developerNameInputtf.   setText("");
+        if (developerNameInputtf.getText().isEmpty()){
+            developerNameInputtf.   setText("");
+            webDevTxtBx.            setSelected(false);
+            javaDevTxtBx.           setSelected(false);
+            mobileDevTxtBx.         setSelected(false);
+            testlbl.                setText("Please enter a valid name");
+        }
+        else{
+            developerNameInputtf.   setText("");
+            webDevTxtBx.            setSelected(false);
+            javaDevTxtBx.           setSelected(false);
+            mobileDevTxtBx.         setSelected(false);
+            registerbtn.            setEnabled(false);
+            testlbl.                setText("Developer registered");
+            finishInputbtn.         setEnabled(true);
+            
+        }
+        
     }//GEN-LAST:event_registerbtnActionPerformed
 
     private void mobileDevTxtBxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mobileDevTxtBxActionPerformed
         // TODO add your handling code here:
-        if (   webDevTxtBx.     isSelected() == false && 
-               javaDevTxtBx.    isSelected()== false &&
-               mobileDevTxtBx.  isSelected() == false
+        if (    webDevTxtBx.     isSelected() == false   && 
+                javaDevTxtBx.    isSelected()== false    &&
+                mobileDevTxtBx.  isSelected() == false
                 ) {
             registerbtn.        setEnabled(false);
         }
@@ -274,9 +286,10 @@ public class ImplementationType extends javax.swing.JFrame {
 
     private void webDevTxtBxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_webDevTxtBxActionPerformed
         // TODO add your handling code here:
-        if (    webDevTxtBx.     isSelected() == false && 
-                javaDevTxtBx.    isSelected()== false &&
+        if (    webDevTxtBx.     isSelected() == false  && 
+                javaDevTxtBx.    isSelected()== false   &&
                 mobileDevTxtBx.  isSelected() == false
+                
         ) {
             registerbtn.        setEnabled(false);
         }
@@ -284,6 +297,18 @@ public class ImplementationType extends javax.swing.JFrame {
             registerbtn.        setEnabled(true);
         }
     }//GEN-LAST:event_webDevTxtBxActionPerformed
+
+    private void finishInputbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_finishInputbtnActionPerformed
+        // TODO add your handling code here:
+            webDevTxtBx.            setEnabled(false);
+            webDevTxtBx.            setSelected(false);
+            javaDevTxtBx.           setEnabled(false);
+            javaDevTxtBx.           setSelected(false);
+            mobileDevTxtBx.         setEnabled(false);
+            mobileDevTxtBx.         setSelected(false);
+            developerNameInputtf.   setEditable(false);
+            showDevsbtn.            setEnabled(true);
+    }//GEN-LAST:event_finishInputbtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -336,6 +361,7 @@ public class ImplementationType extends javax.swing.JFrame {
     private javax.swing.JCheckBox mobileDevTxtBx;
     private javax.swing.JButton registerbtn;
     private javax.swing.JToggleButton showDevsbtn;
+    private javax.swing.JLabel testlbl;
     private javax.swing.JCheckBox webDevTxtBx;
     // End of variables declaration//GEN-END:variables
 }
